6c5094600b87c3505862ddb8b0439f25c54110f5
Carlos manuel  gomez mojica <carlos.gomezm@usantoto.edu.co>
2020-11-20 12:16:06 -0500

    base de datos

---
+++ sql/datos tabla activity.sql
@@ -0,0 +1,15 @@
+insert into activity(activity_id,name_activity,description,cost_by_id,turists_quote)values
+(1,'caminata ecologica','cimata por medio de un sendero donde se logra ver la naturaleza sin dañar la anturaleza',10400,10),
+(2,'canotaje','consiste bajar por un rio hasta un dererminado lugar, esto se hace por medio de un bote llamado kayak',20000,4),
+(3,'entrada a piscina','Accaso a piscinas para su uso dentro de el horario que se establesca',14000,100),
+(4,'buceo','Nadar por un determinado tiempo total mete sumergido, por lo general en el mar',180000,3),
+(5,'cabalgatas',' Los paseos en caballo son bastante populares, es posible conseguir sitios que alquilen caballos casi en cualquier parte del país',15000,2),
+(6,'Las ciclovías y las ciclorutas','se han convertido en actividades cotidianas en algunas ciudades del país. Es una gran oportunidad para salir a hacer ejercicio en familia ya que normalmente están acompañadas de actividades recreativas y clases de gimnasia o aeróbicos',8000,50),
+(7,'Parapente y cometas','Son actividades de deportes de aventura relativamente nuevas en el país. Es posible tomar cursos o volar con el instructor en equipos para 2 personas',65000,2),
+(8,'Escalada y Rappel','Al igual que la anterior son actividades de aventura, algunos centros ofrecen clases y alquiler de equipos',30000,5),
+(9,'Ultralivianos',' En algunas zonas del país. es posible alquilar uno de estos mini aviones y sobrevolar con un piloto todos los atractivos geográficos que ofrece Colombia.',600000,4),
+(10,'Avistamiento de Ballenas','Es posible hacerlo en las costas del Pacífico, hay programas turísticos especialmente diseñados para este fin',30000,15),
+(11,'Ski acuático - Jet Ski',' Es posible encontrar equipos de alquiler en todo el mar caribe y algunos de los principales lagos del país',50000,1),
+(12,'Balsaje','En paseo que se hace en ríos de poco caudal, se puede hacer en una balsa hecha de madera o en neumáticos inflados',10000,6);
+
+select * from activity;
